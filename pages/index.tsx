39588b76301563464e12677565957fe1fb909912
Brandon Tabaska <btabaska@gmail.com>
2022-08-22 17:41:02 -0700

    added gradientLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/graadientLayout";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      roundImage
+      color="gray"
+      subtitle="profile"
+      title="Brandon Tabaska"
+      description="15 Public Playlists"
+      image="https://a-z-animals.com/media/2021/07/Lemur.jpg"
+    >
+      <div>Home Page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
